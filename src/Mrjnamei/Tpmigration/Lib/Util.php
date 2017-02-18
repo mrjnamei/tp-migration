a73090c2c2ab054dbd4eabeb9f61e3b4d8720ca3
Mrj <vardump@foxmail.com>
2017-02-18 10:45:35 +0800

    init project

---
+++ src/Mrjnamei/Tpmigration/Lib/Util.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Mrjnamei\Tpmigration\Lib ;
+
+class Util{
+
+
+	
+
+
+}
---
+++ src/Mrjnamei/Tpmigration/Migration.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+/**
+ * 需求 . 1.创建migration表. 
+ */
+namespace Mrjnamei\Tpmigration;
+
+class Migration {
+	
+	private $path = ""; // 保存文件的位置. 
+
+	public function __construct( $config ){
+		
+	}
+
+
+	public function up(){
+
+	}
+
+
+	public function down(){
+		
+	}
+
+}
